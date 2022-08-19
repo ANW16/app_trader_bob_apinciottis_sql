ac01d4f6325078eacf2d4d3d675894f98afe2956
Steven <sgoolsby88@yahoo.com>
2022-08-18 20:27:24 -0500

    Finished_projected_longevity

--- data/scripts.sql
+++ data/scripts.sql
@@ -18,6 +18,13 @@ Where rating is not null
 Group by genres, install_count
 Order by avg_rating desc
 Limit 10;
+--Top 10 "Apple Store" genres by AVG rating....
+Select distinct primary_genre, round(avg(rating),2) as avg_rating, review_count
+From app_store_apps
+Where rating is not null
+Group by primary_genre, review_count
+Order by avg_rating desc
+Limit 10;
 
 Select distinct money(price), install_count,
     category
@@ -38,7 +45,8 @@ Using (name)
 Where p.name = a.name
 Order By p.name,a.name desc;
 
-Select distinct p.name, p.rating,
+-- Projected Longevity(Apps in both stores AND apps with rating >= 3.5 in both stores)
+Select distinct p.name, round(p.rating*2,0)/2 as Playstore_rating, 
     Case When Round(p.rating*2,0)/2 = '0' Then '1 year'
         When Round(p.rating*2,0)/2 = '0.5' Then '2 years'
         When Round(p.rating*2,0)/2 = '1.0' Then '3 years'
@@ -50,13 +58,8 @@ Select distinct p.name, p.rating,
         When Round(p.rating*2,0)/2 = '4.0' Then '9 years'
         When Round(p.rating*2,0)/2 = '4.5' Then '10 years'
         When Round(p.rating*2,0)/2 = '5.0' Then '10+ years'
-        End as Projected_Longevity
-From play_store_apps as p
-Join app_store_apps as a
-Using (name)
-
---Apple apps projected longevity...
-Select distinct a.name, a.rating,
+        End as Playstore_Projected_Longevity,
+ a.name, Round(a.rating*2,0)/2 as Apple_rating,   
     Case When Round(a.rating*2,0)/2 = '0' Then '1 year'
         When Round(a.rating*2,0)/2 = '0.5' Then '2 years'
         When Round(a.rating*2,0)/2 = '1.0' Then '3 years'
@@ -68,9 +71,11 @@ Select distinct a.name, a.rating,
         When Round(a.rating*2,0)/2 = '4.0' Then '9 years'
         When Round(a.rating*2,0)/2 = '4.5' Then '10 years'
         When Round(a.rating*2,0)/2 = '5.0' Then '10+ years'
-        End as Projected_Longevity
-From app_store_apps as a
-Join play_store_apps as p
+        End as Apple_Projected_Longevity   
+From play_store_apps as p
+Join app_store_apps as a
 Using (name)
-Group by a.name, a.rating
-Order by name desc;
+Where p.rating >= '3.5'
+    And a.rating >= '3.5'
+Group by p.name,p.rating,a.name,a.rating
+Order by p.name,a.name desc;
