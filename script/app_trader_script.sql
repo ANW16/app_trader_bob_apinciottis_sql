81c185fc706cd66274fa2b05ecf039bff6d4eef0
Alex Wright <anwturp@gmail.com>
2022-08-20 12:24:30 -0400

    cleaned code for genre rating to include both stores with simplified naming scheme. Also added a table that finds that ratio of money spent to money earned for each app.

--- script/app_trader_script.sql
+++ script/app_trader_script.sql
@@ -25,12 +25,18 @@ LIMIT 10;
 -- PewDiePie's Tuber Simulator(4.90), ASOS(4.85), and The EO Bar(4.85) are the top 3 .
 
 -- App genres ranked by average rating:
-SELECT a1.primary_genre, ROUND(AVG((a1.rating+g1.rating)/2),2) AS avg_rating
+SELECT 
+CASE
+    WHEN a1.primary_genre = LEFT(g1.genres, POSITION(';' IN g1.genres) - 1) THEN a1.primary_genre
+    ELSE LEFT(g1.genres, POSITION(';' IN g1.genres) - 1)
+END as genre, 
+ROUND(AVG((a1.rating+g1.rating)/2),2) AS avg_rating
 FROM play_store_apps as g1
 INNER JOIN app_store_apps as a1 USING(name)
-GROUP BY a1.primary_genre
+WHERE POSITION(';' IN g1.genres) > 0
+GROUP BY genre
 ORDER BY avg_rating DESC;
--- Top genres are Catalogs(4.55), Books(4.50), Health & Fitness(4.45), Games(4.38), and Productivity(4.32) .
+-- Top genres are Action(4.50), Racing(4.50), Adventure(4.46), Card(4.45), and Puzzle(4.44) .
 
 -- Apps ranked by review count totals:
 SELECT name, COALESCE(CAST(MAX(a1.review_count) AS int),0)+COALESCE(CAST(MAX(g1.review_count) AS int),0) as total_reviews
@@ -114,6 +120,46 @@ revenue AS
 
 SELECT name, 
 money(r1.revenue - (((p1.apple_purchase_price + p1.google_purchase_price) * 10000) + a2.ad_cost)) AS profit
+FROM play_store_apps as g1
+INNER JOIN app_store_apps as a1 USING(name)
+INNER JOIN purchase_price as p1 USING(name)
+INNER JOIN advertising_cost as a2 USING(name)
+INNER JOIN revenue as r1 USING(name)
+GROUP BY name, r1.revenue, p1.apple_purchase_price, p1.google_purchase_price, a2.ad_cost
+ORDER BY profit DESC;
+-- 
+
+
+WITH purchase_price AS
+   (SELECT DISTINCT name,
+    CASE 
+        WHEN money(g1.price) = money(0) THEN money(1)
+        ELSE money(g1.price) 
+    END as google_purchase_price, 
+    CASE 
+        WHEN money(a1.price) = money(0) THEN money(1)
+        ELSE money(a1.price) 
+    END as apple_purchase_price
+    FROM play_store_apps as g1
+    INNER JOIN app_store_apps as a1 USING(name)),
+    
+advertising_cost AS
+   (SELECT name, money(((ROUND((AVG((a1.rating+g1.rating)/2)/.5))+1)*12)*1000) as ad_cost
+    FROM play_store_apps as g1
+    INNER JOIN app_store_apps as a1 USING(name)
+    GROUP BY name
+    ORDER BY ad_cost DESC),
+    
+revenue AS
+   (SELECT name, money(((ROUND((AVG((a1.rating+g1.rating)/2)/.5))+1)*12)*5000) as revenue
+    FROM play_store_apps as g1
+    INNER JOIN app_store_apps as a1 USING(name)
+    GROUP BY name
+    ORDER BY revenue DESC)
+
+SELECT name, 
+(r1.revenue - (((p1.apple_purchase_price + p1.google_purchase_price) * 10000) + a2.ad_cost)) / 
+(((p1.apple_purchase_price + p1.google_purchase_price) * 10000) + a2.ad_cost) AS profit
 FROM play_store_apps as g1
 INNER JOIN app_store_apps as a1 USING(name)
 INNER JOIN purchase_price as p1 USING(name)
